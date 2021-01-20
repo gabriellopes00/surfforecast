3c4a1851e60c048a28cf2c9f78d88e9ab07b7170
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-01-20 19:21:07 -0300

    chore: add jest setup for tests

---
+++ tests/jest.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path')
+const root = resolve(__dirname, '..')
+const rootConfig = require(`${root}/jest.config.js`)
+
+module.exports = {
+  ...rootConfig,
+  ...{
+    rootDir: root,
+    displayName: 'end2end-tests',
+    setupFilesAfterEnv: ['<rootDir>/tests/jest-setup.ts'],
+    testMatch: ['<rootDir>/tests/**/*.test.ts']
+  }
+}
