05f6c33607efd1fb3846e0152a5b0fc80aa8045b
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-01-23 15:43:24 -0300

    refactor: improve .env config to export variables independently

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
-export const env = {
-  port: process.env.PORT,
-  stormGlassToken: process.env.STORM_GLASS_TOKEN
-}
+export const port = process.env.PORT
+export const stormGlassToken = process.env.STORM_GLASS_TOKEN
+export const stormGlassUrl = 'https://api.stormglass.io/v2'
+
+export const env = { port, stormGlassToken, stormGlassUrl }
