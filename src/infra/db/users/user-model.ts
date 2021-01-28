8ab167e8c4508f67c5fad4db96d669836e37680d
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-01-28 13:22:03 -0300

    feat: ensure UsersController generate a hash with user password before isnert into database

--- src/infra/db/users/user-model.ts
+++ src/infra/db/users/user-model.ts
@@ -1,6 +1,8 @@
 import mongoose, { Document, Model } from 'mongoose'
 import { AddUserModel } from '@src/domain/usecases/users/add-user'
 
+import bcrypt from 'bcrypt'
+
 const schema = new mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -29,6 +31,29 @@ schema.path('email').validate(
   'already exists in the database.',
   VALIDATION.DUPLICATED
 )
+
+schema.pre<UserSchema>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return
+  }
+  try {
+    const hashedPassword = await hashPassword(this.password)
+    this.password = hashedPassword
+  } catch (err) {
+    console.error(`Error hashing the password for the user ${this.name}`, err)
+  }
+})
+
+export async function hashPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password, 12)
+}
+
+export async function compare(
+  password: string,
+  hash: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, hash)
+}
 
 export interface UserSchema extends AddUserModel, Document {}
 export const User: Model<UserSchema> = mongoose.model('User', schema)
--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -1,4 +1,4 @@
-import { User } from '@src/infra/db/users/user-model'
+import { compare, User } from '@src/infra/db/users/user-model'
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
@@ -14,7 +14,15 @@ describe('Users functional tests', () => {
       }
       const response = await global.testRequest.post('/users').send(newUser)
       expect(response.status).toBe(201)
-      expect(response.body).toEqual(expect.objectContaining(newUser))
+      await expect(
+        compare(newUser.password, response.body.password)
+      ).resolves.toBeTruthy()
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) }
+        })
+      )
     })
 
     it('Should return 422 when there is a validation error', async () => {
