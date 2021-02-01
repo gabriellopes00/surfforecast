04331dc2500d64caa8ff5e85ec7197535ebf3192
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-01 16:25:07 -0300

    feat: create manually cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,18 +1,19 @@
 import { Express, json, Request, Response, NextFunction } from 'express'
-import cors from 'cors'
 
 const bodyParser = json()
-export const contentType = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const contentType = (_: Request, res: Response, next: NextFunction) => {
   res.type('json')
+  next()
+}
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-headers', '*')
   next()
 }
 
 export default (app: Express): void => {
-  app.use(cors())
+  app.use(cors)
   app.use(bodyParser)
   app.use(contentType)
 }
