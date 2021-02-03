c1cd0488c7f507f7be4b6460601ba3e48f4f6b53
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-03 16:54:14 -0300

    feat: ensure server exitecorrectly depending onf the cause of exit

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,12 +5,46 @@ import logger from '../config/logger'
 
 connect()
   .then(async () => {
-    logger.info('Mongodb connected successfully')
+    try {
+      logger.info('Mongodb connected successfully')
 
-    const app = (await import('./config/index')).default
+      const app = (await import('./config/index')).default
+      const server = app.listen(port, () => {
+        logger.info(`Server running at http://localhost:${port}`)
+      })
 
-    app.listen(port, () => {
-      logger.info(`Server running at http://localhost:${port}`)
-    })
+      const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+      exitSignals.map(sig =>
+        process.on(sig, async () => {
+          try {
+            server.close()
+            logger.info('Server stopped successfully')
+            process.exit(0)
+          } catch (error) {
+            logger.error(`App exited with error: ${error}`)
+            process.exit(1)
+          }
+        })
+      )
+    } catch (error) {
+      logger.error(error)
+      process.exit(1)
+    }
   })
-  .catch(err => console.error(err))
+  .catch(error => {
+    logger.error(error)
+    process.exit(1)
+  })
+
+// Errors handles
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due an unhandled promise: ${promise} and reason: ${reason}`
+  )
+  throw reason
+})
+
+process.on('uncaughtException', error => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`)
+  process.exit(0)
+})
