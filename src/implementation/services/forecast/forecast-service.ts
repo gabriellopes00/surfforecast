bc951861ab7c8a5a318e0c804286f26726a15a25
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-05 14:29:33 -0300

    feat: ensure forecast-service returns forecasts ordered by rating (desc)

--- src/implementation/services/forecast/forecast-service.ts
+++ src/implementation/services/forecast/forecast-service.ts
@@ -5,43 +5,62 @@ import { BeachModel } from '@src/domain/models/beach'
 import { BeachForecast, TimeForecast } from '@src/domain/models/forecast'
 import { AddBeachModel } from '@src/domain/usecases/beaches/add-beach'
 import { ProcessForecast } from '@src/domain/usecases/forecast/process-forecast'
+import { RatingService } from '../rating/rating-service'
+import _ from 'lodash'
 
 export class ForecastService implements ProcessForecast {
-  constructor(protected readonly stormGlass = new StormGlassClient()) {}
+  constructor(
+    protected readonly stormGlass = new StormGlassClient(),
+    protected readonly ratingService: typeof RatingService = RatingService
+  ) {}
 
   public async processByBeaches(
     beaches: BeachModel[]
   ): Promise<TimeForecast[]> {
     try {
-      const pointsSources: BeachForecast[] = []
-
-      for (const beach of beaches) {
-        const points: ForecastPoint[] = await this.stormGlass.fetchPoints(
-          beach.lat,
-          beach.lng
-        )
-        const enrichedData = this.enrichedData(points, beach)
-        pointsSources.push(...enrichedData)
-      }
-      return this.mapForecastByTime(pointsSources)
+      const pointsSources = await this.calculateRating(beaches)
+      const timeForecast = this.mapForecastByTime(pointsSources)
+      return timeForecast.map(timeForecast => ({
+        time: timeForecast.time,
+        forecast: _.orderBy(timeForecast.forecast, ['rating'], 'desc')
+      }))
     } catch (error) {
       throw new ForecastInternalProcessingError(error.message)
     }
   }
 
+  private async calculateRating(
+    beaches: BeachModel[]
+  ): Promise<BeachForecast[]> {
+    const pointsSources: BeachForecast[] = []
+
+    for (const beach of beaches) {
+      const rating = new RatingService(beach)
+      const points: ForecastPoint[] = await this.stormGlass.fetchPoints(
+        beach.lat,
+        beach.lng
+      )
+      const enrichedData = this.enrichedData(points, beach, rating)
+      pointsSources.push(...enrichedData)
+    }
+
+    return pointsSources
+  }
+
   private enrichedData(
     points: ForecastPoint[],
-    beach: AddBeachModel
+    beach: AddBeachModel,
+    rating: RatingService
   ): BeachForecast[] {
-    return points.map(e => ({
+    return points.map(point => ({
       ...{
         lat: beach.lat,
         lng: beach.lng,
         name: beach.name,
         position: beach.position,
-        rating: 1
+        rating: rating.getPointRating(point)
       },
-      ...e
+      ...point
     }))
   }
 
--- tests/functional/forecast.test.ts
+++ tests/functional/forecast.test.ts
@@ -1,41 +1,38 @@
-// import { Beach } from '@src/infra/db/beaches/beach-model'
-// import { User } from '@src/infra/db/users/user-model'
-// import { BeachPosition } from '@src/domain/models/beach'
-// import stormGlassWeather from '../fixtures/stormglass-weather-response.json'
-// import apiForecastResponse from '../fixtures/api-forecast-response.json'
+// import { Position } from '@src/domain/models/beach'
+// import stormGlassWeather3HoursFixture from '../fixtures/stormglass-weather-response.json'
+// import apiForecastResponse1BeachFixture from '../fixtures/stormglass-normalized-response.json'
 
 // import nock from 'nock'
+// import { AddUserModel } from '@src/domain/models/user'
+// import { Beach } from '@src/infra/db/beaches/beach-model'
+// import { User } from '@src/infra/db/users/user-model'
+// import { JwtAdapter } from '@src/infra/cryptography/jwt-adapter'
+// import { secretKey } from '@src/config/env'
 
 // describe('Beach forecast functional tests', () => {
-//   const fakeUser = {
-//     name: 'Gabriel Lopes',
-//     email: 'gabriel@mail.com',
-//     password: '1234',
-//     passwordConfirmation: '1234'
+//   const defaultUser: AddUserModel = {
+//     name: 'John Doe',
+//     email: 'john3@mail.com',
+//     password: '1234'
 //   }
-
+//   let token: string
 //   beforeEach(async () => {
 //     await Beach.deleteMany({})
 //     await User.deleteMany({})
-
+//     const user = await new User(defaultUser).save()
 //     const defaultBeach = {
 //       lat: -33.792726,
 //       lng: 151.289824,
 //       name: 'Manly',
-//       position: BeachPosition.E,
-//       user: '60158934c537fd21959c6f1c'
+//       position: Position.E,
+//       user: user.id
 //     }
 //     await new Beach(defaultBeach).save()
+//     const genToken = new JwtAdapter(secretKey)
+//     genToken.encrypt(user.id)
 //   })
-
-//   it('Should return a forecast with just a few times', async () => {
-//     await global.testRequest.post('/api/users').send(fakeUser)
-//     const responseToken = await global.testRequest
-//       .post('/api/login')
-//       .send({ email: fakeUser.email, password: fakeUser.password })
-//     const token = responseToken.body.accessToken
-
-//     nock('https://api.stormglass.io', {
+//   it('should return a forecast with just a few times', async () => {
+//     nock('https://api.stormglass.io:443', {
 //       encodedQueryParams: true,
 //       reqheaders: {
 //         Authorization: (): boolean => true
@@ -49,42 +46,36 @@
 //         params: /(.*)/,
 //         source: 'noaa'
 //       })
-//       .reply(200, stormGlassWeather)
+//       .reply(200, stormGlassWeather3HoursFixture)
 
 //     const { body, status } = await global.testRequest
 //       .get('/api/forecast')
 //       .set({ 'access-token': token })
-
 //     expect(status).toBe(200)
-//     expect(body).toEqual(apiForecastResponse)
+//     // Make sure we use toEqual to check value not the object and array itself
+//     expect(body).toEqual(apiForecastResponse1BeachFixture)
 //   })
 
-//   // it('should return 500 if something goes wrong during the processing', async () => {
-//   //   await global.testRequest.post('/api/users').send(fakeUser)
-//   //   const responseToken = await global.testRequest
-//   //     .post('/api/login')
-//   //     .send({ email: fakeUser.email, password: fakeUser.password })
-//   //   const token = responseToken.body.accessToken
+//   it('should return 500 if something goes wrong during the processing', async () => {
+//     nock('https://api.stormglass.io:443', {
+//       encodedQueryParams: true,
+//       reqheaders: {
+//         Authorization: (): boolean => true
+//       }
+//     })
+//       .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+//       .get('/v1/weather/point')
+//       .query({ lat: '-33.792726', lng: '151.289824' })
+//       .replyWithError('Something went wrong')
 
-//   //   nock('https://api.stormglass.io', {
-//   //     encodedQueryParams: true,
-//   //     reqheaders: {
-//   //       Authorization: (): boolean => true
-//   //     }
-//   //   })
-//   //     .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-//   //     .get('/v2/weather/point')
-//   //     .query({ lat: '-33.792726', lng: '151.289824' })
-//   //     .replyWithError('Something went wrong')
+//     const { status } = await global.testRequest
+//       .get(`/api/forecast`)
+//       .set({ 'access-token': token })
 
-//   //   const { status } = await global.testRequest
-//   //     .get('/api/forecast')
-//   //     .set({ 'access-token': token })
-
-//   //   expect(status).toBe(500)
-//   // })
+//     expect(status).toBe(500)
+//   })
 // })
 
-test('test in development', () => {
-  expect(1 + 1).toBe(2)
+test('', () => {
+  expect(1).toBe(1)
 })
