723dad61d069e017476fb16a312b72663d881f9d
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-05 16:12:06 -0300

    feat: add api rate limiter

--- src/main/routes/forecast.ts
+++ src/main/routes/forecast.ts
@@ -1,8 +1,27 @@
-import { Router } from 'express'
+import { Request, Router, Response } from 'express'
+import rateLimit from 'express-rate-limit'
 import { adaptRoutes } from '../adapters/express-routes'
 import { forecastController } from '../compositions/controllers/forecast-controller'
 
 import { AuthMiddleware } from '@src/presentation/middlewares/auth'
+
+const RateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 3,
+  keyGenerator(req: Request): string {
+    return req.ip
+  },
+  handler(_, res: Response): void {
+    res
+      .status(400)
+      .json({ error: 'Too many requests to the /forecast endpoint' })
+  }
+})
 export default (router: Router): void => {
-  router.get('/forecast', AuthMiddleware, adaptRoutes(forecastController))
+  router.get(
+    '/forecast',
+    RateLimiter,
+    AuthMiddleware,
+    adaptRoutes(forecastController)
+  )
 }
